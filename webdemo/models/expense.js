5c415ec54094dd93a0c2f8e5cd251b37ecdcd69b
thanapat <thanapat.wir@g.swu.ac.th>
2026-02-24 11:31:57 +0700

    Done Lab webdemo

---
+++ webdemo/models/expense.js
@@ -0,0 +1,68 @@
+class Expense {
+  /**
+   Represents a Expense.
+   @constructor
+   @param {date} date - The date of the expense.
+   @param {float} income - The author of the book.
+   @param {number} expense - The expense amount.
+   @param {string} detail - The details or description of the transaction.
+ */
+  constructor(date, income, expense, detail) {
+    this.date = date;
+    this.income = parseFloat(income) || 0;
+    this.expense = parseFloat(expense) || 0;
+    this.detail = detail || '';
+  }
+}
+
+/**
+ * Class ExpenseModel.
+ * @class
+ */
+class ExpenseModel {
+  constructor() {
+    this.expenses = [];
+  }
+
+/**
+ * add expense numbers passed to the function.
+ * @param {float} expense - A positive number.
+ */
+  add(expense) {
+    this.expenses.push(expense);
+  }
+
+/**
+ * return expense numbers from stack.
+ * @return {array} expense - array of positive number
+ */
+  getAll() {
+    return this.expenses;
+  }
+
+/**
+ * Calculate the total income from all records.
+ * @return {number} The sum of all income.
+ */
+  getTotalIncome() {
+    return this.expenses.reduce((sum, exp) => sum + exp.income, 0);
+  }
+
+/**
+ * Calculate the total expense from all records.
+ * @return {number} The sum of all expenses.
+ */
+  getTotalExpense() {
+    return this.expenses.reduce((sum, exp) => sum + exp.expense, 0);
+  }
+
+/**
+ * Calculate the remaining money (Total Income - Total Expense).
+ * @return {number} The calculated remaining balance.
+ */
+  getMoneyLeft() {
+    return this.getTotalIncome() - this.getTotalExpense();
+  }
+}
+
+module.exports = { Expense, ExpenseModel };
